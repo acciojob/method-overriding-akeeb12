1dc2afb43d9a5ea63ca97dd5ca4b0a3e1bd895c4
Akeeb1 <akeeebmalligawad605@gmail.com>
2024-05-14 19:11:24 +0530

    Encapsulation -1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,19 @@
 package com.driver;
 
-class B extends A
-{
+class A{
     public String meth()
     {
-        return "Method is overridden in Extended class B";
+        return "Invoking method from class A";
     }
+}
+class B extends A{
+
 }
 public class Main {
   public static  void main(String[] args)
   {
       B objB = new B();
+      System.out.println(objB.meth());
       System.out.println(objB.meth());
   }
 }
