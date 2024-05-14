231919a8f0e10f2855437f96ea9b69158b1b7f43
Akeeb1 <akeeebmalligawad605@gmail.com>
2024-05-14 19:24:56 +0530

    Encapsulation -1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,23 @@
 package com.driver;
 
-class A{
-    public String meth()
-    {
+class A {
+    public String meth() {
         return "Invoking method from class A";
     }
 }
-class B extends A{
 
+class B extends A {
+    // Task 4: Override meth() method of class A in class B
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
 }
+
 public class Main {
-  public static  void main(String[] args)
-  {
-      B objB = new B();
-      System.out.println(objB.meth());
-      System.out.println(objB.meth());
-  }
+    public static void main(String[] args) {
+        // Task 3: Create an object of class B and call method meth of class A with it
+        A objB = new B();
+        System.out.println(objB.meth()); // This will call the overridden method from class B
+    }
 }
