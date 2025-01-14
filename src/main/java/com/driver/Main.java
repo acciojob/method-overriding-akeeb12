7de8284198b77f80e439dcf1703460c8f62d074b
akeeb12 <akeebmm@gmail.com>
2025-01-14 16:31:01 +0530

    Code added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,14 @@
 package com.driver;
 
-class A {
+public class Main {
+public static class A {
     public String meth() {
+
         return "Invoking method from class A";
     }
 }
 
-class B extends A {
+public static class B extends A {
     // Task 4: Override meth() method of class A in class B
     @Override
     public String meth() {
@@ -14,10 +16,13 @@ class B extends A {
     }
 }
 
-public class Main {
+
     public static void main(String[] args) {
         // Task 3: Create an object of class B and call method meth of class A with it
-        A objA = new B();
-        System.out.println(objA.meth()); // This will call the overridden method from class B
+        B obj = new B();
+        obj.meth();
+        
+        B objA = new B();
+        objA.meth(); // This will call the overridden method from class B
     }
 }
